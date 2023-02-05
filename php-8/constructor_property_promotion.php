534fb8d003791dcad9e17f272818fd1931cfdb8a
Nguyen <nguyenuit@gmail.com>
2023-02-05 23:03:37 +0700

    php 8, constructor property promotion, arrange project structure

---
+++ php-8/constructor_property_promotion.php
@@ -0,0 +1,11 @@
+<?php
+class Point {
+    public function __construct(
+        public float $x = 0.1,
+        public float $y = 0.2,
+        public float $z = 0.3
+    ){
+        // ...
+    }
+}
+?>
